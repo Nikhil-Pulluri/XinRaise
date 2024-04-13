312f668df0b9be22344ba634019ee1b3d5c1d376
ashfaqjani916 <ashfaqjani916@gmail.com>
2024-04-14 02:49:36 +0530

    added xrc20 contract to mint a new token

--- xdc-hardhat/hardhat.config.ts
+++ xdc-hardhat/hardhat.config.ts
@@ -2,9 +2,15 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+
+
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
+const APOTHEM_NETWORK_URL = process.env.APOTHEM_NETWORK_URL;
 const XINFIN_NETWORK_URL = process.env.XINFIN_NETWORK_URL;
 const XINFIN_PRIVATE_KEY = process.env.XINFIN_PRIVATE_KEY;
 
@@ -15,11 +21,30 @@ module.exports = {
       url: XINFIN_NETWORK_URL,
       accounts: [XINFIN_PRIVATE_KEY],
     },
+    apothem:{
+      url: APOTHEM_NETWORK_URL,
+      accounts: [XINFIN_PRIVATE_KEY],
+    },
   },
 };
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    xinfin: {
+      url: process.env.XINFIN_NETWORK_URL,
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+    apothem: {
+      url: process.env.APOTHEM_NETWORK_URL,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  }
 };
+
+
+dotenv.config();
+
+
 
 export default config;
--- xdc-hardhat/scripts/deploy.ts
+++ xdc-hardhat/scripts/deploy.ts
@@ -1,30 +1,53 @@
-const hre = require("hardhat");
+// const hre = require("hardhat");
+
+// async function main() {
+//   // make sure to change the name of your contract
+  
+//   const Campaign = await hre.ethers.getContractFactory("CrowdFunding");
+//   // 4 in the bracket is to give the value to the parameter(_pizzaSize) in the constructor of the smart contract contract.
+//   const campaign = await Campaign.deploy();
+
+
+//   // const campaign = await Campaign.attach("0x6b197d95Dc89D4f0118B37c08bc2fdD2E192b8f4");
+
+//   await campaign.waitForDeployment();
+//   // const numberofCampaigns = await campaign.createCampaign("0x6b197d95Dc89D4f0118B37c08bc2fdD2E192b8f4","Dimo Hacks","This is a cool project that we are building for dimo hacks","23","1713097011","https://jade-immense-parrotfish-172.mypinata.cloud/ipfs/QmWPn53epYHSyR376EiHupy6AWfT4CoN4uqMbZoSWY7FBX");
+
+//   // console.log("Campaign creation successfull",numberofCampaigns);
+
+//   // const allcampaigns = campaign.getCampaigns();
+//   // console.log("Campaigns are:",{allcampaigns});
+
+//   console.log("pizza contract address:", campaign);
+// }
+
+// // Call the main function and catch if there is any error
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
+
+
+
+
+import { ethers } from "hardhat";
 
 async function main() {
-  // make sure to change the name of your contract
+  const XRC20 = await ethers.getContractFactory("XRC20Token");
+  const myToken = await XRC20.deploy("MyToken", "MTK", 18, 1000);
+
   
-  const Campaign = await hre.ethers.getContractFactory("CrowdFunding");
-  // 4 in the bracket is to give the value to the parameter(_pizzaSize) in the constructor of the smart contract contract.
-  // const campaign = await Campaign.deploy();
+  await myToken.waitForDeployment();
 
-
-  const campaign = await Campaign.attach("0x6b197d95Dc89D4f0118B37c08bc2fdD2E192b8f4");
-
-  // await campaign.waitForDeployment();
-  const numberofCampaigns = await campaign.createCampaign("0x6b197d95Dc89D4f0118B37c08bc2fdD2E192b8f4","Dimo Hacks","This is a cool project that we are building for dimo hacks","23","1713097011","https://jade-immense-parrotfish-172.mypinata.cloud/ipfs/QmWPn53epYHSyR376EiHupy6AWfT4CoN4uqMbZoSWY7FBX");
-
-  console.log("Campaign creation successfull",numberofCampaigns);
-
-  // const allcampaigns = campaign.getCampaigns();
-  // console.log("Campaigns are:",{allcampaigns});
-
-  // console.log("pizza contract address:", campaign);
+  console.log("Token Successfully Deployed!");
+  console.log(`Token address: `,myToken );
 }
 
-// Call the main function and catch if there is any error
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
