e36dd76bc0081c89e1f8ee92eac09539b572083a
ashfaqjani916 <ashfaqjani916@gmail.com>
2024-04-14 01:06:21 +0530

    added campaign contract

--- xdc-hardhat/scripts/deploy.ts
+++ xdc-hardhat/scripts/deploy.ts
@@ -2,13 +2,23 @@ const hre = require("hardhat");
 
 async function main() {
   // make sure to change the name of your contract
-  const Pizza = await hre.ethers.getContractFactory("Pizza");
+  
+  const Campaign = await hre.ethers.getContractFactory("CrowdFunding");
   // 4 in the bracket is to give the value to the parameter(_pizzaSize) in the constructor of the smart contract contract.
-  const pizza = await Pizza.deploy(4);
+  // const campaign = await Campaign.deploy();
 
-  await pizza.waitForDeployment();
 
-  console.log("pizza contract address:", pizza);
+  const campaign = await Campaign.attach("0x6b197d95Dc89D4f0118B37c08bc2fdD2E192b8f4");
+
+  // await campaign.waitForDeployment();
+  const numberofCampaigns = await campaign.createCampaign("0x6b197d95Dc89D4f0118B37c08bc2fdD2E192b8f4","Dimo Hacks","This is a cool project that we are building for dimo hacks","23","1713097011","https://jade-immense-parrotfish-172.mypinata.cloud/ipfs/QmWPn53epYHSyR376EiHupy6AWfT4CoN4uqMbZoSWY7FBX");
+
+  console.log("Campaign creation successfull",numberofCampaigns);
+
+  // const allcampaigns = campaign.getCampaigns();
+  // console.log("Campaigns are:",{allcampaigns});
+
+  // console.log("pizza contract address:", campaign);
 }
 
 // Call the main function and catch if there is any error
